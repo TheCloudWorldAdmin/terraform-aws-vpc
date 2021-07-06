6513bdb2a2d105f9c58dc2b004ba7728af0bfa6a
wilshan <74950445+wilshan@users.noreply.github.com>
2021-07-06 18:37:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -125,7 +125,7 @@ resource "aws_vpc_dhcp_options_association" "dhcp_options_association" {
 ################################################################################
 
 resource "aws_internet_gateway" "myIGW" {
-  count = var.create_vpc && var.create_igw && var.public_subnets > 0 ? 1 : 0
+  count = var.create_vpc && var.create_igw && lenght(var.public_subnets) > 0 ? 1 : 0
 
   vpc_id = aws_vpc.myVPC.id
 
@@ -196,38 +196,33 @@ resource "aws_default_route_table" "default_route_table" {
 # Publiс routes
 ################################################################################
 
-resource "aws_route_table" "public" {
+resource "aws_route_table" "public_route_table" {
   count = var.create_vpc && length(var.public_subnets) > 0 ? 1 : 0
 
-  vpc_id = local.vpc_id
+  vpc_id = aws_vpc.myVPC.id
 
-  tags = merge(
-    {
-      "Name" = format("%s-${var.public_subnet_suffix}", var.name)
-    },
-    var.tags,
-    var.public_route_table_tags,
-  )
+  tags = {
+    "Name" = var.public_route_table_tag
 }
 
 resource "aws_route" "public_internet_gateway" {
   count = var.create_vpc && var.create_igw && length(var.public_subnets) > 0 ? 1 : 0
 
-  route_table_id         = aws_route_table.public[0].id
+  route_table_id         = aws_route_table.public_route_table.id
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.this[0].id
+  gateway_id             = aws_internet_gateway.myIGW.id
 
   timeouts {
-    create = "5m"
+    create = "10m"
   }
 }
 
 resource "aws_route" "public_internet_gateway_ipv6" {
   count = var.create_vpc && var.create_igw && var.enable_ipv6 && length(var.public_subnets) > 0 ? 1 : 0
 
-  route_table_id              = aws_route_table.public[0].id
+  route_table_id              = aws_route_table.public_route_table.id
   destination_ipv6_cidr_block = "::/0"
-  gateway_id                  = aws_internet_gateway.this[0].id
+  gateway_id                  = aws_internet_gateway.myIGW.id
 }
 
 ################################################################################
