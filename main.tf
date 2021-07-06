e939e50d4cb177e4804a1fc67389cb67aa0e6eba
wilshan <74950445+wilshan@users.noreply.github.com>
2021-07-06 16:14:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -40,7 +40,12 @@ locals {
   from_port = var.ingress_from_port
   to_port = var.ingress_to_port
   protocol = var.ingress_protocol
-}]
+  ipv6_cidr_blocks = var.ingress_ipv6_cidr_blocks
+  prefix_list_ids = var.ingress_prefix_list_ids
+  security_groups = var.ingress_security_groups
+},
+{ 
+  
 }
 
 resource "aws_default_security_group" "this" {
@@ -51,11 +56,11 @@ resource "aws_default_security_group" "this" {
   dynamic "ingress" {
     for_each = var.default_security_group_ingress
     content {
-      self             = 
-      cidr_blocks      = 
-      ipv6_cidr_blocks = 
-      prefix_list_ids  = 
-      security_groups  = 
+      self             = lookup(ingress.value, "self", null)
+      cidr_blocks      = lookup(ingress.value, "cidr_blocks", null)
+      ipv6_cidr_blocks = lookup(ingress.value, "ingress_ipv6_cidr_blocks", null)
+      prefix_list_ids  = lookup(ingress.value, "prefix_list_ids", null)
+      security_groups  = lookup(ingress.value, "security_groups", null)
       description      = lookup(ingress.value, "description", null)
       from_port        = lookup(ingress.value, "from_port", 0)
       to_port          = lookup(ingress.value, "to_port", 0)
