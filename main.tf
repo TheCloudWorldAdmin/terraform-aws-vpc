c28c565f951f9cae8a348ff7837bcf7c0d6821b5
wilshan <74950445+wilshan@users.noreply.github.com>
2021-07-07 13:56:53 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -203,7 +203,7 @@ resource "aws_eip" "nat_eip" {
 }
 }
 resource "aws_nat_gateway" "my_nat" {
-  allocation_id = aws_eip.my_eip.id
+  allocation_id = aws_eip.nat_eip.id
   subnet_id     = aws_subnet.database.*.id
 
   tags = {
