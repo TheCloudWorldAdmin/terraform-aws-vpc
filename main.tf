2511af37c380f77f86116646cf5e3613587cb9be
wilshan <74950445+wilshan@users.noreply.github.com>
2021-07-07 16:13:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -609,87 +609,39 @@ resource "aws_network_acl_rule" "database_outbound" {
 ################################################################################
 
 resource "aws_route_table_association" "private_route_table_association" {
-  count = var.create_vpc && length(var.private_subnets) > 0 ? length(var.private_subnets) : 0
+  count = var.create_vpc && var.private_route_table_association_required ? 1: 0
 
   subnet_id = element(aws_subnet.private.*.id, count.index)
   route_table_id = element(
     aws_route_table.private.*.id,
-    var.single_nat_gateway ? 0 : count.index,
+ #   var.single_nat_gateway ? 0 : count.index,
   )
 }
 
-resource "aws_route_table_association" "outpost" {
-  count = var.create_vpc && length(var.outpost_subnets) > 0 ? length(var.outpost_subnets) : 0
-
-  subnet_id = element(aws_subnet.outpost.*.id, count.index)
-  route_table_id = element(
-    aws_route_table.private.*.id,
-    var.single_nat_gateway ? 0 : count.index,
-  )
-}
 
 resource "aws_route_table_association" "database" {
-  count = var.create_vpc && length(var.database_subnets) > 0 ? length(var.database_subnets) : 0
+ count = var.create_vpc && var.database_route_table_association_required ? 1: 0
 
   subnet_id = element(aws_subnet.database.*.id, count.index)
   route_table_id = element(
     coalescelist(aws_route_table.database.*.id, aws_route_table.private.*.id),
-    var.create_database_subnet_route_table ? var.single_nat_gateway || var.create_database_internet_gateway_route ? 0 : count.index : count.index,
+#    var.create_database_subnet_route_table ? var.single_nat_gateway || var.create_database_internet_gateway_route ? 0 : count.index : count.index,
   )
 }
 
-resource "aws_route_table_association" "redshift" {
-  count = var.create_vpc && length(var.redshift_subnets) > 0 && false == var.enable_public_redshift ? length(var.redshift_subnets) : 0
-
-  subnet_id = element(aws_subnet.redshift.*.id, count.index)
-  route_table_id = element(
-    coalescelist(aws_route_table.redshift.*.id, aws_route_table.private.*.id),
-    var.single_nat_gateway || var.create_redshift_subnet_route_table ? 0 : count.index,
-  )
-}
-
-resource "aws_route_table_association" "redshift_public" {
-  count = var.create_vpc && length(var.redshift_subnets) > 0 && var.enable_public_redshift ? length(var.redshift_subnets) : 0
-
-  subnet_id = element(aws_subnet.redshift.*.id, count.index)
-  route_table_id = element(
-    coalescelist(aws_route_table.redshift.*.id, aws_route_table.public.*.id),
-    var.single_nat_gateway || var.create_redshift_subnet_route_table ? 0 : count.index,
-  )
-}
-
-resource "aws_route_table_association" "elasticache" {
-  count = var.create_vpc && length(var.elasticache_subnets) > 0 ? length(var.elasticache_subnets) : 0
-
-  subnet_id = element(aws_subnet.elasticache.*.id, count.index)
-  route_table_id = element(
-    coalescelist(
-      aws_route_table.elasticache.*.id,
-      aws_route_table.private.*.id,
-    ),
-    var.single_nat_gateway || var.create_elasticache_subnet_route_table ? 0 : count.index,
-  )
-}
-
-resource "aws_route_table_association" "intra" {
-  count = var.create_vpc && length(var.intra_subnets) > 0 ? length(var.intra_subnets) : 0
-
-  subnet_id      = element(aws_subnet.intra.*.id, count.index)
-  route_table_id = element(aws_route_table.intra.*.id, 0)
-}
 
 resource "aws_route_table_association" "public" {
-  count = var.create_vpc && length(var.public_subnets) > 0 ? length(var.public_subnets) : 0
-
+  count = var.create_vpc && var.public_route_table_association_required ? 1: 0
+  
   subnet_id      = element(aws_subnet.public.*.id, count.index)
-  route_table_id = aws_route_table.public[0].id
+  route_table_id = aws_route_table.public_route_table.id
 }
 
 ################################################################################
 # Defaults
 ################################################################################
 
-resource "aws_default_vpc" "this" {
+resource "aws_default_vpc" "default_vpc" {
   count = var.manage_default_vpc ? 1 : 0
 
   enable_dns_support   = var.default_vpc_enable_dns_support
