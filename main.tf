8fbc931ee4de6c145e8349cfed60eb5d02da02ae
wilshan <74950445+wilshan@users.noreply.github.com>
2021-07-07 11:52:49 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,8 +3,6 @@
 ################################################################################
 
 resource "aws_vpc" "myVPC" {
-  count = var.create_vpc ? 1 : 0
-
   cidr_block                       = var.cidr
   instance_tenancy                 = var.instance_tenancy
   enable_dns_hostnames             = var.enable_dns_hostnames
