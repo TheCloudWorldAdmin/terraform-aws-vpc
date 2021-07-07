33fc21904b39dbdf6ce06dafdd48544823a9d71e
wilshan <74950445+wilshan@users.noreply.github.com>
2021-07-07 14:15:57 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -382,7 +382,7 @@ resource "aws_db_subnet_group" "database_subnet_group" {
 
   name        = var.db_subnet_group_name
   description = "Database subnet group for ${var.db_subnet_group_name}"
-  subnet_ids  = aws_subnet.database.id
+  subnet_ids  = [aws_subnet.database.id]
 
   tags = {
     "Name" = var.db_subnet_group_name
