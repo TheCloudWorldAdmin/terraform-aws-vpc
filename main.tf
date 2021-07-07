5f9d60afd31f30246412c10504aa7b1f0adffa04
wilshan <74950445+wilshan@users.noreply.github.com>
2021-07-07 14:01:10 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -204,7 +204,7 @@ resource "aws_eip" "nat_eip" {
 }
 resource "aws_nat_gateway" "my_nat" {
   allocation_id = aws_eip.nat_eip.id
-  subnet_id     = aws_subnet.database.*.id
+  subnet_id     = aws_subnet.database.id
 
   tags = {
     Name = "gw NAT"
