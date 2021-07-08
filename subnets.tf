a36d2c2d367bcaca451c219d0ee01e1cf77268d9
wilshan <74950445+wilshan@users.noreply.github.com>
2021-07-08 12:47:21 +0530

    Update subnets.tf

--- subnets.tf
+++ subnets.tf
@@ -84,7 +84,7 @@ resource "aws_db_subnet_group" "database_subnet_group" {
   
  #for_each = aws_subnet.database[count.index]
  #subnet_ids = [each.value]
- subnet_ids  = [for s in aws_subnet.database.ids : s.id]
+ subnet_ids  = [for s in aws_subnet.database[count.index] : s.id]
   name        = var.db_subnet_group_name
   description = "Database subnet group for ${var.db_subnet_group_name}"
   tags = {
