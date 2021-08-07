31826c3924a51534edb5ded05346a9cdd8ffcea8
wilshan <74950445+wilshan@users.noreply.github.com>
2021-08-07 20:51:50 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,3 +1,7 @@
 provider "aws" {
-  region = var.region_name
+  version = "~> 3.0"
+  region  = "us-east-1"
+    assume_role {
+    role_arn     = "arn:aws:iam::667481606687:role/programatic-access"
+  }
 }
